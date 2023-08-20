d79fad42a4036fe2d1750e9e16769ad2a39c4bb5
Mohamed <s2000588@edu.bc.fi>
2023-08-20 11:24:00 +0300

    create navebar & footer

--- src/app/Cart/page.tsx
+++ src/app/Cart/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
-  )
-}
+    <Link href='/cart' className='flex items-center gap-4'>
+      <div className='relative w-8 h-8'>
+        <Image src='/cart.png' alt='Cart' fill />
+      </div>
+      <span>Cart (5)</span>
+    </Link>
+  );
+};
 
-export default CartPage
+export default CartPage;
---
+++ src/components/Menu.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { links } from "@/links/Links";
+import CartPage from "@/app/cart/page";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  // teporary user
+  const user = false;
+  return (
+    <div>
+      {!isOpen ? (
+        <Image
+          src='/open.png'
+          alt='open logo'
+          width={20}
+          height={20}
+          onClick={() => setIsOpen(true)}
+        />
+      ) : (
+        <Image
+          src='/close.png'
+          alt='close logo'
+          width={20}
+          height={20}
+          onClick={() => setIsOpen(false)}
+        />
+      )}
+      {isOpen && (
+        <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10'>
+          {links.map((link) => (
+            <Link
+              href={link.url}
+              key={link.id}
+              onClick={() => setIsOpen(false)}
+            >
+              {link.title}
+            </Link>
+          ))}
+          {!user ? (
+            <Link href='/login' onClick={() => setIsOpen(false)}>
+              Login
+            </Link>
+          ) : (
+            <Link href='/orders' onClick={() => setIsOpen(false)}>
+              Orders
+            </Link>
+          )}
+          <Link href='/cart' onClick={() => setIsOpen(false)}>
+            <CartPage />
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Menu;
