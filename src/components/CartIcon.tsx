a5b7b244f9cfad9df01a8d14bc075b8ee43fd8eb
Mohamed <s2000588@edu.bc.fi>
2023-08-21 17:21:18 +0300

    update Navbar & create cart component

---
+++ src/components/CartIcon.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const CartIcon = () => {
+  return (
+    <Link href='/cart' className='flex items-center gap-4'>
+      <div className='relative w-8 h-8'>
+        <Image src='/cart.png' alt='Cart' fill />
+      </div>
+      <span>Cart (5)</span>
+    </Link>
+  );
+};
+
+export default CartIcon;
